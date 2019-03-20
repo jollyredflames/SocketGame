da01b5fa53782746afc42eb8a3fcb9883865f550
Sarthak Saxena <ssarthak.1999@gmail.com>
2019-03-20 14:41:11 -0400

    Added annotations

--- a3/helper.c
+++ a3/helper.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "helper.h"
 
-
+/*Return file size given its name*/
 int get_file_size(char *filename) {
     struct stat sbuf;
 
@@ -46,7 +46,7 @@ int set_up_how_many_records_each_child_reads(int *how_many_records_arr, int proc
     return num_records;
 }
 
-/*Given a child, return how many bytes from beginning of in file this child should start to read Records from*/
+/*Given a child, return how many bytes from beginning of file this child should start to read Records from*/
 int getOffset(int child_responsibility, int* words_per_child){
     int offset = 0;
     for(int i = 0; i < child_responsibility; i++){
@@ -55,6 +55,7 @@ int getOffset(int child_responsibility, int* words_per_child){
     return offset;
 }
 
+/*given # processes pipe, find the smallest record and write to file. Once the smallest Record from processes is written, read from 'this' pipe again (the pipe that gave the last smallest Record.) If the pipe has no more records, set the pipe to -1 so we know to not sort this record. otherwise run again.*/
 void mergeSortPipeAndWrite(Rec *holder, int processes, int actual_size, int total_records, FILE *outfp, int pipe_arr[processes][2]){
     int count = 0;
     while(count < total_records){
