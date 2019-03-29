ca8552fcb5b0e83348d0aed4432d1bcd1bd4e9e2
Sarthak Saxena <ssarthak.1999@gmail.com>
2019-03-29 11:05:49 -0400

    LAST LAB\?\!\?\!\?\! This shit bananas

--- lab11/chat_client.c
+++ lab11/chat_client.c
@@ -8,18 +8,24 @@
 
 
 #ifndef PORT
-  #define PORT 30000
+#define PORT 56831
 #endif
 #define BUF_SIZE 128
 
 int main(void) {
+    // Read username
+    char username[BUF_SIZE +1];
+    int numb_read = read(STDIN_FILENO, username, BUF_SIZE);
+    
+    username[numb_read - 1] = '\0';
+    
     // Create the socket FD.
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd < 0) {
         perror("client: socket");
         exit(1);
     }
-
+    
     // Set the IP and port of the server to connect to.
     struct sockaddr_in server;
     server.sin_family = AF_INET;
@@ -29,36 +35,66 @@ int main(void) {
         close(sock_fd);
         exit(1);
     }
-
+    
     // Connect to the server.
     if (connect(sock_fd, (struct sockaddr *)&server, sizeof(server)) == -1) {
         perror("client: connect");
         close(sock_fd);
         exit(1);
     }
-
+    
+    
+    int numb_written = write(sock_fd, username, numb_read);
+    if (numb_written != numb_read) {
+        perror("Client username write");
+        close(sock_fd);
+        exit(1);
+    }
     // Read input from the user, send it to the server, and then accept the
     // echo that returns. Exit when stdin is closed.
+    int max_fd = sock_fd;
+    fd_set all_fds;
+    FD_ZERO(&all_fds);
+    FD_SET(sock_fd, &all_fds);
+    FD_SET(STDIN_FILENO, &all_fds);
+    
     char buf[BUF_SIZE + 1];
     while (1) {
-        int num_read = read(STDIN_FILENO, buf, BUF_SIZE);
-        if (num_read == 0) {
-            break;
-        }
-        buf[num_read] = '\0';         
-
-        int num_written = write(sock_fd, buf, num_read);
-        if (num_written != num_read) {
-            perror("client: write");
-            close(sock_fd);
+        fd_set listen_fds = all_fds;
+        int nready = select(max_fd + 1, &listen_fds, NULL, NULL, NULL);
+        if (nready == -1) {
+            perror("client: select");
             exit(1);
         }
-
-        num_read = read(sock_fd, buf, BUF_SIZE);
-        buf[num_read] = '\0';
-        printf("Received from server: %s", buf);
+        
+        if (FD_ISSET(sock_fd, &listen_fds)) {
+            int num_read = read(sock_fd, buf, BUF_SIZE);
+            buf[num_read] = '\0';
+            if (num_read == 0 ){
+                fprintf(stderr, "Failed to read from server");
+                exit(1);
+            }
+  
+            printf("Received from server: %s", buf);
+        }
+        
+        if (FD_ISSET(STDIN_FILENO, &listen_fds)) {
+            int num_read = read(STDIN_FILENO, buf, BUF_SIZE);
+            if (num_read == 0) {
+                break;
+            }
+            buf[num_read] = '\0';
+            
+            int num_written = write(sock_fd, buf, num_read);
+            if (num_written != num_read) {
+                perror("client: write");
+                close(sock_fd);
+                exit(1);
+            }
+            
+        }
     }
-
+    
     close(sock_fd);
     return 0;
 }
--- lab11/chat_server.c
+++ lab11/chat_server.c
@@ -9,7 +9,7 @@
 
 
 #ifndef PORT
-  #define PORT 30000
+#define PORT 30000
 #endif
 #define MAX_BACKLOG 5
 #define MAX_CONNECTIONS 12
@@ -32,21 +32,24 @@ int accept_connection(int fd, struct sockname *usernames) {
     while (user_index < MAX_CONNECTIONS && usernames[user_index].sock_fd != -1) {
         user_index++;
     }
-
+    
     if (user_index == MAX_CONNECTIONS) {
         fprintf(stderr, "server: max concurrent connections\n");
         return -1;
     }
-
+    
     int client_fd = accept(fd, NULL, NULL);
     if (client_fd < 0) {
         perror("server: accept");
         close(fd);
         exit(1);
     }
-
+    usernames[user_index].username = malloc(sizeof(char) * 30);
+    
+    read(client_fd, usernames[user_index].username, 30 * sizeof(char));
+    // printf("Username: %s \n ", usernames[user_index].username);
     usernames[user_index].sock_fd = client_fd;
-    usernames[user_index].username = NULL;
+    //usernames[user_index].username = NULL;
     return client_fd;
 }
 
@@ -57,14 +60,30 @@ int accept_connection(int fd, struct sockname *usernames) {
 int read_from(int client_index, struct sockname *usernames) {
     int fd = usernames[client_index].sock_fd;
     char buf[BUF_SIZE + 1];
-
-    int num_read = read(fd, &buf, BUF_SIZE);
-    buf[num_read] = '\0';
+    
+    strcpy(buf, usernames[client_index].username);
+    strcat(buf,": ");
+    int username_size = strlen(usernames[client_index].username) + (2 * sizeof(char));
+    char read_buf[BUF_SIZE +1 - username_size];
+    
+    int num_read = read(fd, &read_buf, BUF_SIZE);
+    
+    strcat(buf, read_buf);
+    buf[num_read + username_size] = '\0';
+    for(int i = 0; i < MAX_CONNECTIONS; i++) {
+        int curr_fd = usernames[i].sock_fd;
+        if (fd == curr_fd) {
+            continue;
+        }
+        else if (num_read == 0 || write( curr_fd, buf,  strlen(buf)) != strlen(buf)) {
+            
+        }
+    }
     if (num_read == 0 || write(fd, buf, strlen(buf)) != strlen(buf)) {
         usernames[client_index].sock_fd = -1;
         return fd;
     }
-
+    
     return 0;
 }
 
@@ -75,45 +94,45 @@ int main(void) {
         usernames[index].sock_fd = -1;
         usernames[index].username = NULL;
     }
-
+    
     // Create the socket FD.
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd < 0) {
         perror("server: socket");
         exit(1);
     }
-
+    
     // Set information about the port (and IP) we want to be connected to.
     struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr.s_addr = INADDR_ANY;
-
+    
     // This should always be zero. On some systems, it won't error if you
     // forget, but on others, you'll get mysterious errors. So zero it.
     memset(&server.sin_zero, 0, 8);
-
+    
     // Bind the selected port to the socket.
     if (bind(sock_fd, (struct sockaddr *)&server, sizeof(server)) < 0) {
         perror("server: bind");
         close(sock_fd);
         exit(1);
     }
-
+    
     // Announce willingness to accept connections on this socket.
     if (listen(sock_fd, MAX_BACKLOG) < 0) {
         perror("server: listen");
         close(sock_fd);
         exit(1);
     }
-
+    
     // The client accept - message accept loop. First, we prepare to listen to multiple
     // file descriptors by initializing a set of file descriptors.
     int max_fd = sock_fd;
     fd_set all_fds;
     FD_ZERO(&all_fds);
     FD_SET(sock_fd, &all_fds);
-
+    
     while (1) {
         // select updates the fd_set it receives, so we always use a copy and retain the original.
         fd_set listen_fds = all_fds;
@@ -122,7 +141,7 @@ int main(void) {
             perror("server: select");
             exit(1);
         }
-
+        
         // Is it the original socket? Create a new connection ...
         if (FD_ISSET(sock_fd, &listen_fds)) {
             int client_fd = accept_connection(sock_fd, usernames);
@@ -132,7 +151,7 @@ int main(void) {
             FD_SET(client_fd, &all_fds);
             printf("Accepted connection\n");
         }
-
+        
         // Next, check the clients.
         // NOTE: We could do some tricks with nready to terminate this loop early.
         for (int index = 0; index < MAX_CONNECTIONS; index++) {
@@ -148,7 +167,7 @@ int main(void) {
             }
         }
     }
-
+    
     // Should never get here.
     return 1;
 }
